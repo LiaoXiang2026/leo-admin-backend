4f5d7380d5cbcfe57f758bc41b696760da5fe7ca
thence <2668812066@qq.com>
2026-05-05 14:28:00 +0800

    chore: register AppController in AppModule and remove docker-compose.yml
    - Add AppController and AppService to AppModule (fixes health endpoint 404)
    - Remove docker-compose.yml (not needed for Zeabur deployment)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { MenuModule } from './modules/menu/menu.module';
 import { UserModule } from './modules/user/user.module';
@@ -17,6 +19,8 @@ import { PrismaModule } from './prisma/prisma.module';
     UserModule,
     MenuModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export class AppModule {}

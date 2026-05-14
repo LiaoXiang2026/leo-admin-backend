4a9d6862fed0aa05162bac7f23ee8f0b0a9d8ae5
thence <2668812066@qq.com>
2026-05-14 21:35:03 +0800

    chore: temporarily disable IP whitelist middleware

--- src/main.ts
+++ src/main.ts
@@ -13,8 +13,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // IP / 域名白名单（全局最先执行）
-  const ipWhitelist = new IpWhitelistMiddleware();
-  app.use(ipWhitelist.use.bind(ipWhitelist));
+  // const ipWhitelist = new IpWhitelistMiddleware();
+  // app.use(ipWhitelist.use.bind(ipWhitelist));
 
   // 全局前缀
   app.setGlobalPrefix('api');
